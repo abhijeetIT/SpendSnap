eede1b8e1a59fabb91cfce4191c67c7342b7dcd1
abhijeetIT <17333375+abhijeetIT@users.noreply.github.com>
2026-02-14 15:28:49 +0530

    COnfig a /auth to all position where error occure

--- src/main/java/com/spendsnap/SpendsnapApplication.java
+++ src/main/java/com/spendsnap/SpendsnapApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpendsnapApplication {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		SpringApplication.run(SpendsnapApplication.class, args);
 	}
 }
